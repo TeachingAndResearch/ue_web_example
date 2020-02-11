329ea4f325f808f39538c409ce3192f01cdb5726
Jonathan <jonathan.pastor@me.com>
2020-02-11 13:55:30 +0100

    save work

--- app.py
+++ app.py
@@ -12,20 +12,39 @@ def traitement_formulaire_addition(form):
     return resultat
 
 
-def afficher_formulaire_addition(form):
+def afficher_formulaire_addition(form, errors):
     return flask.render_template("form_addition.html.jinja2")
+
+
+def formulaire_est_valide(form):
+    number_a = flask.request.form.get("number_a")
+    number_b = flask.request.form.get("number_b")
+    operator = flask.request.form.get("operator")
+
+    result = True
+    errors = []
+
+    if number_a is None:
+        result = False
+        errors += ["missing 'number_a' parameter"]
+    if number_b is None:
+        result = False
+        errors += ["missing 'number_b' parameter"]
+    if operator is None:
+        result = False
+        errors += ["missing 'operator' parameter"]
+
+    return result, errors
 
 
 @app.route("/add", methods=["GET", "POST"])
 @app.route("/", methods=["GET", "POST"])
 def fonction_formulaire_addition():
 
-    number_a = flask.request.form.get("number_a")
-    number_b = flask.request.form.get("number_b")
-    operator = flask.request.form.get("operator")
+    form_est_valide, errors = formulaire_est_valide(flask.request.form)
 
-    if number_a is None or number_b is None or operator is None:
-        return afficher_formulaire_addition(flask.request.form)
+    if not form_est_valide:
+        return afficher_formulaire_addition(flask.request.form, errors)
     else:
         return traitement_formulaire_addition(flask.request.form)
 
