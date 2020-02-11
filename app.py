2d02f1406fb80d5c47b57b2664a432f3cf765877
Jonathan <jonathan.pastor@me.com>
2020-02-11 11:02:09 +0100

    save work for correction of session on forms

--- app.py
+++ app.py
@@ -7,17 +7,27 @@ app.config.from_object(Config)
 
 
 def traitement_formulaire_addition(form):
-    pass
+    expression = f"{form.get('number_a')} {form.get('operator')} {form.get('number_b')}"
+    resultat = "%s" % eval(expression)
+    return resultat
 
 
 def afficher_formulaire_addition(form):
-    pass
+    return flask.render_template("form_addition.html.jinja2")
 
 
 @app.route("/add", methods=["GET", "POST"])
 @app.route("/", methods=["GET", "POST"])
 def fonction_formulaire_addition():
-    return "TODO: implement me!"
+
+    number_a = flask.request.form.get("number_a")
+    number_b = flask.request.form.get("number_b")
+    operator = flask.request.form.get("operator")
+
+    if number_a is None or number_b is None or operator is None:
+        return afficher_formulaire_addition(flask.request.form)
+    else:
+        return traitement_formulaire_addition(flask.request.form)
 
 
 if __name__ == '__main__':
