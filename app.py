f5fbb3c0e6ee203ea291434f3b834d72c1276d92
Jonathan <jonathan.pastor@me.com>
2020-02-26 12:22:21 +0100

    save work

--- app.py
+++ app.py
@@ -1,95 +1,65 @@
-from flask import Flask
 import flask
-from database.database import db, init_database
+from database.models import *
 
-from database.models import Task, TaskList
-from misc.forms import NewTaskForm
-
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "secret_key1234"
 
+
 db.init_app(app)
 
 with app.test_request_context():
-    init_database()
+    db.create_all()
 
 
-def save_object_to_db(db_object):
-    db.session.add(db_object)
+# @app.route("/clean")
+def clean():
+    db.drop_all()
+    db.create_all()
+    return "Cleaned!"
+
+
+@app.route("/test")
+def test():
+    clean()
+
+    # Create two sports
+    judo = Sport(name="judo")
+    football = Sport(name="football")
+    db.session.add(judo)
+    db.session.add(football)
     db.session.commit()
 
-
-def remove_object_from_db(db_object):
-    db.session.delete(db_object)
+    # Create three players
+    player1 = Player(firstname="Jonathan", lastname="Pastor")
+    player2 = Player(firstname="Hervé", lastname="Dupont")
+    player3 = Player(firstname="André", lastname="Laroute")
+    db.session.add(player1)
+    db.session.add(player2)
+    db.session.add(player3)
     db.session.commit()
 
+    # Create a team with the two players
+    rocket = Team(name="rocket", sport=judo)
+    sonette = Team(name="sonette", sport=football)
+    db.session.add(rocket)
+    db.session.add(sonette)
+    db.session.commit()
 
-def add_task_to_tasks_list(form, tasks_list):
-    # TODO: implement me
-    return None
+    # Add the two players to the team 'rocket'
+    rocket.players.append(player1)
+    rocket.players.append(player2)
+    db.session.add(rocket)
+    sonette.players.append(player2)
+    sonette.players.append(player3)
+    db.session.commit()
 
+    # Fetch all sports
+    sports = Sport.query.all()
 
-def create_tasks_list(tasks_list_name):
-    # TODO: implement me
-    return None
-
-
-def find_tasks_list_by_name(tasks_list_name):
-    # TODO: implement me
-    return None
-
-
-def find_task_by_id(task_id):
-    # TODO: implement me
-    return None
-
-
-@app.route("/<tasks_list_name>", methods=["GET", "POST"])
-@app.route("/", methods=["GET", "POST"])
-def homepage(tasks_list_name="default"):
-    form = NewTaskForm()
-
-    tasks_list = find_tasks_list_by_name(tasks_list_name)
-
-    if tasks_list is None:
-        tasks_list = create_tasks_list(tasks_list_name)
-
-    if form.validate_on_submit():
-        add_task_to_tasks_list(form, tasks_list)
-        return flask.redirect(flask.url_for("homepage",
-                                            tasks_list_name=tasks_list.name))
-
-    return flask.render_template("show_tasks.html.jinja2",
-                                 tasks_list=tasks_list,
-                                 form=form)
-
-
-@app.route("/toggle_task/<int:task_id>")
-def toggle_task(task_id):
-    existing_task = find_task_by_id(task_id)
-    tasks_list = existing_task.tasks_list
-
-    existing_task.isDone = not existing_task.isDone
-
-    save_object_to_db(existing_task)
-
-    return flask.redirect(flask.url_for("homepage",
-                                        tasks_list_name=tasks_list.name))
-
-
-@app.route("/delete_task/<int:task_id>")
-def delete_task(task_id):
-    existing_task = find_task_by_id(task_id)
-
-    tasks_list = existing_task.tasks_list
-
-    remove_object_from_db(existing_task)
-
-    return flask.redirect(flask.url_for("homepage",
-                                        tasks_list_name=tasks_list.name))
+    return flask.render_template("index.jinja2", sports=sports)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- misc/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired, ValidationError
-
-
-class NewTaskForm(Form):
-    label = StringField('Ajouter une tâche', validators=[DataRequired()])
