5ee3ff2b89a4ade8a33922d2d4e4b32a0a5abf6a
Marie <Marie-Donnie@users.noreply.github.com>
2021-02-09 20:14:45 +0100

    Update forms.py
    Forms are deprecated, FlaskForm replace them

--- misc/forms.py
+++ misc/forms.py
@@ -1,7 +1,7 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, ValidationError
 
 
-class NewTaskForm(Form):
+class NewTaskForm(FlaskForm):
     label = StringField('Ajouter une tâche', validators=[DataRequired()])
