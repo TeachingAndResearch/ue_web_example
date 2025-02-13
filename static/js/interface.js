63962956d39be503971e0c33a9a5cf1339003dac
Remous-Aris Koutsiamanis <1357969+ariskou@users.noreply.github.com>
2025-02-13 18:41:43 +0100

    Increase the difficulty

--- static/js/interface.js
+++ static/js/interface.js
@@ -2,3 +2,12 @@
 // using $.getJSON
 
 // 2. Populate the 'friends' table in the page using $().append
+
+
+// 3. Take into account the namefilter input: only show the friends that contain the
+// text in the namefilter input. The search should be case insensitive.
+// The search should be done on the server side by passing the namefilter as a query parameter.
+
+
+// 4. Do the search as the user types in the namefilter input. This means that the search
+// should be done on every keyup event.
